dee7af6162dc16b9bdaf5cf15eb569429ef53e83
tMasaaa <noumodulity@gmail.com>
2018-06-13 13:31:28 +0000

    [update] 10.3.2

--- project/sample_app/test/integration/users_edit_test.rb
+++ project/sample_app/test/integration/users_edit_test.rb
@@ -16,10 +16,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_nil session[:forwarding_url]
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: {name: name,
@@ -32,4 +33,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  
 end
